add2e2fa2e5213da030c26054036a6f9020a02af
Ayush Shrestha <ayushrestha05@gmail.com>
2021-12-05 20:39:53 +0545

    Created ThemeData file.

---
+++ lib/utilities/theme_data.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+ThemeData light = ThemeData(
+    colorScheme: const ColorScheme.light(
+  primary: Color(0xFF103388),
+));
+
+ThemeData dark = ThemeData(
+    colorScheme: const ColorScheme.dark(
+  primary: Color(0xFF6FFFE9),
+  surface: Color(0xFF0B132B), //For Headers
+));
