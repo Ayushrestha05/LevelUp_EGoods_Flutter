12733a2ccf4f4cc07a9501e4a71e31b818145160
Ayush Shrestha <ayushrestha05@gmail.com>
2021-12-08 14:05:11 +0545

    Updated Dark Theme Data

--- lib/utilities/theme_data.dart
+++ lib/utilities/theme_data.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:levelup_egoods/utilities/size_config.dart';
 
 ThemeData light = ThemeData(
     colorScheme: const ColorScheme.light(
@@ -6,7 +7,10 @@ ThemeData light = ThemeData(
 ));
 
 ThemeData dark = ThemeData(
-    colorScheme: const ColorScheme.dark(
-  primary: Color(0xFF6FFFE9),
-  surface: Color(0xFF0B132B), //For Headers
-));
+  buttonTheme: const ButtonThemeData(
+      buttonColor: Color(0xFF103388), textTheme: ButtonTextTheme.primary),
+  colorScheme: const ColorScheme.dark(
+    primary: Color(0xFF6FFFE9),
+    surface: Color(0xFF0B132B),
+  ),
+);
