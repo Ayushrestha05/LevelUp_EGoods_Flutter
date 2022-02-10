56ea191438647c63b572ae1498da5551e5f4e5f0
Ayush Shrestha <ayushrestha05@gmail.com>
2022-02-11 00:15:08 +0545

    Updated Theme data

--- lib/utilities/theme_data.dart
+++ lib/utilities/theme_data.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 
 ThemeData light = ThemeData(
-    colorScheme: const ColorScheme.light(
-  primary: Color(0xFF103388),
-));
+  scaffoldBackgroundColor: const Color(0xFFF6F6F6),
+  bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+    selectedItemColor: Color(0xFF6FFFE9),
+    unselectedItemColor: Color(0xFFB8BDC9),
+    backgroundColor: Color(0xFF112149),
+  ),
+  colorScheme: const ColorScheme.light(
+    primary: Color(0xFF103388),
+  ),
+);
 
 ThemeData dark = ThemeData(
+  bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+    selectedItemColor: Color(0xFF6FFFE9),
+    unselectedItemColor: Color(0xFFB8BDC9),
+    backgroundColor: Color(0xFF112149),
+  ),
   buttonTheme: const ButtonThemeData(
       buttonColor: Color(0xFF103388), textTheme: ButtonTextTheme.primary),
   colorScheme: const ColorScheme.dark(
