85f51a8bc06b372e6f12d5dba03fbf4180a74b20
Ayush Shrestha <ayushrestha05@gmail.com>
2022-02-12 17:35:58 +0545

    Created UserHandler that uses SharedPreferences to save user data locally.

---
+++ lib/utilities/user_handler.dart
@@ -0,0 +1,22 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserHandler {
+  void saveLogin(String userName, String userEmail, String userToken) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('user_name', userName);
+    await prefs.setString('user_email', userEmail);
+    await prefs.setString('user_token', userToken);
+  }
+
+  Future<String?> getUserToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('user_token');
+  }
+
+  void loggedOut() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('user_name');
+    await prefs.remove('user_email');
+    await prefs.remove('user_token');
+  }
+}
