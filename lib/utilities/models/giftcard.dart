03fca12c31779080b7451584e0c7a95633d64b43
Ayush Shrestha <ayushrestha05@gmail.com>
2022-02-24 14:59:55 +0545

    Updated GiftCard Model to support more Methods

--- lib/utilities/models/giftcard.dart
+++ lib/utilities/models/giftcard.dart
@@ -7,21 +7,46 @@ import 'package:levelup_egoods/utilities/constants.dart';
 class GiftCard with ChangeNotifier {
   int _id = 0;
   String _itemName = '', _itemDescription = '', _itemImage = '';
+  var _cardDetails = [];
+  int _isSelected = 0;
 
   get id => _id;
   get itemName => _itemName;
   get itemDescription => _itemDescription;
   get itemImage => _itemImage;
+  get isSelected => _isSelected;
+  get cardDetails => _cardDetails;
 
   GiftCard(int itemID) {
     _id = itemID;
+    getCardDetails();
   }
 
   void getCardDetails() async {
-    var response = await http.get(Uri.parse('$apiUrl/items/music-data/$id'),
+    var response = await http.get(Uri.parse('$apiUrl/items/gift-card-data/$id'),
         headers: {'Accept': 'application/json'});
 
-    var decode = jsonDecode(response.body);
-    print(decode);
+    var decode = jsonDecode(response.body)[0];
+    _itemName = decode['item_details']['item_name'];
+    _itemDescription = decode['item_details']['item_description'];
+    _itemImage = decode['item_details']['item_image'];
+    _cardDetails = decode['gift_card_details'];
+    _isSelected = _cardDetails[0]['id'];
+    notifyListeners();
+  }
+
+  void setSelected(int id) {
+    _isSelected = id;
+    notifyListeners();
+  }
+
+  String getSelectedPrice() {
+    for (int i = 0; i < _cardDetails.length; i++) {
+      if (_cardDetails[i]['id'] == _isSelected) {
+        return _cardDetails[i]['card_price'].toString();
+      }
+    }
+
+    return 'NaN';
   }
 }
