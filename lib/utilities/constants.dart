5c0218151ae9758077323d056d4c6e645004a25f
Ayush Shrestha <ayushrestha05@gmail.com>
2022-03-19 13:27:17 +0545

    Added new function ta provides Shadows for Widgets

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,1 +1,22 @@
-const String apiUrl = "http://192.168.1.71:8000/api";
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const String apiUrl = "http://192.168.1.72:8000/api";
+
+getBoxShadow(BuildContext context) {
+  return Theme.of(context).scaffoldBackgroundColor.value == 4292929004
+      ? const [
+          BoxShadow(
+              color: Color(0xFFA3B1C6), offset: Offset(9, 9), blurRadius: 12),
+          BoxShadow(
+              color: Colors.white, offset: Offset(-9, -9), blurRadius: 12),
+        ]
+      : [
+          BoxShadow(
+              color: Colors.black.withOpacity(0.4),
+              offset: const Offset(9, 9),
+              blurRadius: 12),
+          const BoxShadow(
+              color: Color(0xB33A3A3A), offset: Offset(-9, -9), blurRadius: 12),
+        ];
+}
