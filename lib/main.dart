2203ff130386ecd7acdf1821379c66ec741ef13a
Ayush Shrestha <ayushrestha05@gmail.com>
2022-03-19 13:28:59 +0545

    Updated Single Provider to MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:levelup_egoods/screens/base_screen.dart';
 import 'package:levelup_egoods/utilities/auth.dart';
+import 'package:levelup_egoods/utilities/models/theme.dart';
 import 'package:levelup_egoods/utilities/theme_data.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(
-      create: (BuildContext context) => Auth(), child: const MyApp()));
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+        create: (BuildContext context) => Auth(),
+      ),
+      ChangeNotifierProvider<AppTheme>(
+        create: (BuildContext context) => AppTheme(),
+      ),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -16,14 +26,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final theme = Provider.of<AppTheme>(context);
     //Setting the App to Portrait Only
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: light,
+      debugShowCheckedModeBanner: false,
+      title: 'Level Up EGoods',
+      theme: theme.selectedThemeData,
       home: BaseScreen(),
     );
   }
