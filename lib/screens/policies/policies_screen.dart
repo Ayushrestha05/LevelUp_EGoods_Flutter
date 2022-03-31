b15efc395258ceb1cee51d71548b0a0053b5364f
Ayush Shrestha <ayushrestha05@gmail.com>
2022-03-31 17:06:29 +0545

    Added Policies Screen

---
+++ lib/screens/policies/policies_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class PoliciesScreen extends StatelessWidget {
+  const PoliciesScreen({Key? key}) : super(key: key);
+  static const TextStyle _textStyle = TextStyle(fontFamily: 'Archivo');
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Policies',
+              style: _textStyle,
+            ),
+            centerTitle: true,
+            bottom: const TabBar(
+              tabs: [
+                Tab(
+                  text: 'Privacy Policy',
+                ),
+                Tab(text: 'Terms and Conditions')
+              ],
+            ),
+          ),
+          body: const TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: [
+              WebView(
+                javascriptMode: JavascriptMode.unrestricted,
+                initialUrl: 'https://www.privacypolicytemplate.net/',
+              ),
+              WebView(
+                javascriptMode: JavascriptMode.unrestricted,
+                initialUrl:
+                    'https://www.termsfeed.com/blog/sample-terms-and-conditions-template/',
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
